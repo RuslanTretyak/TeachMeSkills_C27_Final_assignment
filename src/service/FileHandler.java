c6190138b44f426d799b1871d2e5609a9f453c5b
Ruslan <ruslan.tretyak.94@gmail.com>
2024-03-12 00:39:15 +0300

    add CurrencyConverter

--- src/service/FileHandler.java
+++ src/service/FileHandler.java
@@ -11,6 +11,7 @@ import java.math.BigDecimal;
 
 public class FileHandler {
     public static void handleFiles(File directory) {
+        Logger.writeExecuteLog("folder processing started");
         boolean isDirectoryPathCorrect = false;
         try {
             isDirectoryPathCorrect = FileHandlerUtils.checkInputDirectory(directory);
@@ -22,14 +23,15 @@ public class FileHandler {
             BigDecimal checkStatistic = new BigDecimal(0);
             BigDecimal invoiceStatistic = new BigDecimal(0);
             BigDecimal orderStatistic = new BigDecimal(0);
+            Logger.writeExecuteLog("getting statistics on checks");
             try {
                 checkStatistic = FileHandlerUtils.processFiles(FileNamePattern.CHECK_NAME_PATTERN,
                         FileHandlerUtils.getDataFromFiles(FileHandlerUtils.
                                 getFilesFromDirectory(directory.getPath() + "\\checks",
                                         FileNamePattern.CHECK_NAME_PATTERN)));
             } catch (EmptyDirectoryException e) {
-                System.out.println(e.getMessage());
                 Logger.writeExceptionLog(e.getMessage());
+                System.out.println(e.getMessage());
             }
             try {
                 invoiceStatistic = FileHandlerUtils.processFiles(FileNamePattern.INVOICE_NAME_PATTERN,
@@ -37,8 +39,8 @@ public class FileHandler {
                                 getFilesFromDirectory(directory.getPath() + "\\invoices",
                                         FileNamePattern.INVOICE_NAME_PATTERN)));
             } catch (EmptyDirectoryException e) {
-                System.out.println(e.getMessage());
                 Logger.writeExceptionLog(e.getMessage());
+                System.out.println(e.getMessage());
             }
             try {
                 orderStatistic = FileHandlerUtils.processFiles(FileNamePattern.ORDER_NAME_PATTERN,
@@ -46,13 +48,17 @@ public class FileHandler {
                                 getFilesFromDirectory(directory.getPath() + "\\orders",
                                         FileNamePattern.ORDER_NAME_PATTERN)));
             } catch (EmptyDirectoryException e) {
-                System.out.println(e.getMessage());
                 Logger.writeExceptionLog(e.getMessage());
+                System.out.println(e.getMessage());
             }
             BigDecimal totalStatistic = checkStatistic.add(invoiceStatistic).add(orderStatistic);
+            Logger.writeExecuteLog("recording general statistics in the statistics file");
             StatisticWriter.writeDataToStatisticFile("total turnover for the year", totalStatistic.toString());
+            Logger.writeExecuteLog("recording statistics on invoices in the statistics file");
             StatisticWriter.writeDataToStatisticFile("total turnover for all invoices", invoiceStatistic.toString());
+            Logger.writeExecuteLog("recording statistics on orders in the statistics file");
             StatisticWriter.writeDataToStatisticFile("total turnover for all orders", orderStatistic.toString());
+            Logger.writeExecuteLog("recording statistics on checks in the statistics file");
             StatisticWriter.writeDataToStatisticFile("total turnover for all checks", checkStatistic.toString());
         }
     }
--- src/util/FileHandlerUtils.java
+++ src/util/FileHandlerUtils.java
@@ -7,7 +7,6 @@ import exception.IncorrectDirectoryPathException;
 import logger.Logger;
 import validation.FileContentValidator;
 import validation.FileNameValidator;
-
 import java.io.*;
 import java.math.BigDecimal;
 import java.util.*;
@@ -17,21 +16,29 @@ import java.util.stream.Collectors;
 
 public class FileHandlerUtils {
     public static boolean checkInputDirectory(File directory) throws IncorrectDirectoryPathException {
+        Logger.writeExecuteLog("checking the entered path");
         if (!directory.isDirectory()) {
+            Logger.writeExecuteLog("check failed (Exception): the object at the entered path is not a folder");
             throw new IncorrectDirectoryPathException("the object at the entered path is not a folder");
         } else if (directory.listFiles().length == 0) {
+            Logger.writeExecuteLog("check failed (Exception): the folder at the entered path is empty");
             throw new IncorrectDirectoryPathException("the folder at the entered path is empty");
         } else if (!(new File(directory + "\\checks").exists())) {
+            Logger.writeExecuteLog("check failed (Exception): There is no \"checks\" folder at the specified path");
             throw new IncorrectDirectoryPathException("There is no \"checks\" folder at the specified path");
         } else if (!(new File(directory + "\\invoices").exists())) {
+            Logger.writeExecuteLog("check failed (Exception): There is no \"invoices\" folder at the specified path");
             throw new IncorrectDirectoryPathException("There is no \"invoices\" folder at the specified path");
         } else if (!(new File(directory + "\\orders").exists())) {
+            Logger.writeExecuteLog("check failed (Exception): There is no \"orders\" folder at the specified path");
             throw new IncorrectDirectoryPathException("There is no \"orders\" folder at the specified path");
         }
+        Logger.writeExecuteLog("check passed");
         return true;
     }
 
     public static List<File> getFilesFromDirectory(String directoryPath, FileNamePattern fileNamePattern) throws EmptyDirectoryException {
+        Logger.writeExecuteLog("getting files '" + fileNamePattern + "' from a folder");
         File file = new File(directoryPath);
         List<File> rowListOfFiles;
         List<File> resultListOfFiles = new ArrayList<>();
@@ -48,6 +55,7 @@ public class FileHandlerUtils {
                     }
                 }
             } else {
+                Logger.writeExecuteLog("getting files '" + fileNamePattern + "' failed (Exception): the folder '" + directoryPath + "' is empty");
                 throw new EmptyDirectoryException("the folder '" + directoryPath + "' is empty");
             }
         }
@@ -57,18 +65,22 @@ public class FileHandlerUtils {
     public static Map<String, List<String>> getDataFromFiles(List<File> listOfFiles) {
         Map<String, List<String>> resultData = new HashMap<>();
         for (File tempFile : listOfFiles) {
+            Logger.writeExecuteLog("getting data from a file '" + tempFile.getName() + "'");
             try (BufferedReader bufferedReader = new BufferedReader(new FileReader(tempFile))) {
                 resultData.put(tempFile.getAbsolutePath(),
                         bufferedReader.lines().map(String::trim).collect(Collectors.toList()));
             } catch (FileNotFoundException e) {
-                System.out.println("file '" + tempFile + "' not found");
+                Logger.writeExecuteLog("getting data from a file '" + tempFile.getName() + "' failed (Exception): file '" + tempFile + "' not found");
                 Logger.writeExceptionLog("file '" + tempFile + "' not found");
+                System.out.println("file '" + tempFile + "' not found");
             } catch (IOException e) {
-                System.out.println("file '" + tempFile + "' access error");
+                Logger.writeExecuteLog("getting data from a file '" + tempFile.getName() + "' failed (Exception): file '" + tempFile + "' access error");
                 Logger.writeExceptionLog("file '" + tempFile + "' access error");
+                System.out.println("file '" + tempFile + "' access error");
             } catch (Exception e) {
-                System.out.println("error while reading file '" + tempFile + "'");
+                Logger.writeExecuteLog("getting data from a file '" + tempFile.getName() + "' error while reading file '" + tempFile + "'");
                 Logger.writeExceptionLog("error while reading file '" + tempFile + "'");
+                System.out.println("error while reading file '" + tempFile + "'");
             }
         }
         return resultData;
@@ -77,6 +89,7 @@ public class FileHandlerUtils {
     public static BigDecimal processFiles(FileNamePattern fileNamePattern, Map<String, List<String>> listOfData) {
         BigDecimal sum = new BigDecimal(0);
         for (Map.Entry<String, List<String>> entry : listOfData.entrySet()) {
+            Logger.writeExecuteLog("getting statistic from a file '" + entry.getKey() + "'");
             switch (fileNamePattern) {
                 case CHECK_NAME_PATTERN:
                     try {
@@ -120,6 +133,7 @@ public class FileHandlerUtils {
     }
 
     private static boolean moveFile(File fileToMove, FilePathConst pathToMove) {
+        Logger.writeExecuteLog("moving a file '" + fileToMove.getName() + "' to 'non_valid' folder");
         File newFile = new File(pathToMove.getPath() + "\\" + fileToMove.getName());
         boolean isFileCreated = false;
         boolean isFileCopied = false;
@@ -127,11 +141,13 @@ public class FileHandlerUtils {
         try {
             isFileCreated = newFile.createNewFile();
         } catch (IOException e) {
-            System.out.println("file '" + newFile.getName() + "' access error during creation");
+            Logger.writeExecuteLog("moving a file '" + fileToMove.getName() + "' to 'non_valid' folder failed: Exception");
             Logger.writeExceptionLog("file '" + newFile.getName() + "' access error");
+            System.out.println("file '" + newFile.getName() + "' access error during creation");
         } catch (Exception e) {
-            System.out.println("error during creation file '" + newFile.getName() + "'");
+            Logger.writeExecuteLog("moving a file '" + fileToMove.getName() + "' to 'non_valid' folder failed: Exception");
             Logger.writeExceptionLog("error while writing file '" + newFile.getName() + "'");
+            System.out.println("error during creation file '" + newFile.getName() + "'");
         }
         if (isFileCreated) {
             try (FileInputStream fileInputStream = new FileInputStream(fileToMove);
@@ -140,21 +156,26 @@ public class FileHandlerUtils {
                 isFileCopied = true;
 
             } catch (FileNotFoundException e) {
-                System.out.println("files '" + fileToMove.getName() + "/" + newFile.getName() + "' not found during copying");
+                Logger.writeExecuteLog("moving a file '" + fileToMove.getName() + "' to 'non_valid' folder failed: Exception");
                 Logger.writeExceptionLog("files '" + fileToMove.getName() + "/" + newFile.getName() + "' not found during copying");
+                System.out.println("files '" + fileToMove.getName() + "/" + newFile.getName() + "' not found during copying");
             } catch (IOException e) {
-                System.out.println("files '" + fileToMove.getName() + "/" + newFile.getName() + "' access error during copying");
+                Logger.writeExecuteLog("moving a file '" + fileToMove.getName() + "' to 'non_valid' folder failed: Exception");
                 Logger.writeExceptionLog("files '" + fileToMove.getName() + "/" + newFile.getName() + "' access error during copying");
+                System.out.println("files '" + fileToMove.getName() + "/" + newFile.getName() + "' access error during copying");
             } catch (Exception e) {
-                System.out.println("error while copying file '" + fileToMove.getName() + "'");
+                Logger.writeExecuteLog("moving a file '" + fileToMove.getName() + "' to 'non_valid' folder failed: Exception");
                 Logger.writeExceptionLog("error while copying file '" + fileToMove.getName() + "'");
+                System.out.println("error while copying file '" + fileToMove.getName() + "'");
             }
         }
         isFileDeleted = fileToMove.delete();
+        Logger.writeExecuteLog("file '" + fileToMove.getName() + "' has been moved to 'non_valid' folder");
         return isFileCreated && isFileCopied && isFileDeleted;
     }
 
     private static BigDecimal getAmount(List<String> lines, String amountPattern) {
+        Logger.writeExecuteLog("getting amount value");
         BigDecimal result = new BigDecimal(0);
         String amountLine = "0";
         for (String line : lines) {
@@ -162,18 +183,36 @@ public class FileHandlerUtils {
                 amountLine = line;
             }
         }
-        if (amountLine.substring((amountLine.length() - 3), amountLine.length() - 2).equals(",")) {
-            amountLine = amountLine.replace(".", "");
-            amountLine = amountLine.replace(",", ".");
-        } else {
-            amountLine = amountLine.replaceAll(",", "");
-        }
-        Pattern pattern = Pattern.compile("[0-9(,)*]+(.[0-9]{2})?");
+        Pattern pattern = Pattern.compile("[0-9(,|.)*]+((.|,)[0-9]{2})?");
         Matcher matcher = pattern.matcher(amountLine);
-
         if (matcher.find()) {
-            result = new BigDecimal(matcher.group());
+            String sumStr = matcher.group();
+            if (sumStr.length() > 2) {
+                if (sumStr.substring((sumStr.length() - 3), sumStr.length() - 2).equals(",")) {
+                    sumStr = sumStr.replace(".", "");
+                    sumStr = sumStr.replace(",", ".");
+                } else {
+                    sumStr = sumStr.replaceAll(",", "");
+                }
+            }
+            Logger.writeExecuteLog("value '" + sumStr + "' received");
+            result = new BigDecimal(sumStr);
         }
-        return result;
+        String currency = "";
+        pattern = Pattern.compile(" ?(\\$|(USD)) ?");
+        matcher = pattern.matcher(amountLine);
+        if (matcher.find()) {
+            currency = "USD";
+        } else {
+            pattern = Pattern.compile(" ?[A-Z]{3} ?");
+            matcher = pattern.matcher(amountLine);
+            if (matcher.find()) {
+                currency = matcher.group().trim();
+            } else {
+                currency = "USD";
+            }
+        }
+        Logger.writeExecuteLog("value '" + currency + "' received");
+        return CurrencyConverter.convertCurrency(result, currency);
     }
 }
